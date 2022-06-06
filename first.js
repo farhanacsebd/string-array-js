554d7196c05e139949a4a865abefe2c007674a62
farhanacsebd <farhana.cse.bd@gmail.com>
2022-06-06 23:58:39 +0600

    mathmatical js

--- first.js
+++ first.js
@@ -2,7 +2,16 @@
 
 var price1=10;
 var price2=20;
+
+// price1=price1 + 10;
+price1 +=10
+console.log(price1);
+
+price2 -=10;
+price2--;
+console.log(price2);
+
 var total=price1+price2;
-price1+=10;
 console.log(total);
+
 
---
+++ variable-type.js
@@ -0,0 +1,10 @@
+var num = 100;
+console.log(typeof num);
+
+
+var name = "Putul";
+console.log(typeof name);
+
+
+var quantity = true;
+console.log(typeof quantity);
