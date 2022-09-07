2999ea7539f7c6a4c6bb8477e5b813d272fa96ce
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-07 23:31:23 +0600

    case-convert using touppercase

---
+++ case-convert.js
@@ -0,0 +1,6 @@
+const userInput = 'blackPinK';
+const namePro ='blackpink';
+// console.log(namePro.toUpperCase());
+if(userInput.toUpperCase() == namePro.toUpperCase()){
+    console.log('user exists');
+}
