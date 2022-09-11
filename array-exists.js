febaee89430513cada8c765d979133dac89bbbd9
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-12 01:01:43 +0600

    max of list

--- array-exists.js
+++ array-exists.js
@@ -1,4 +1,19 @@
-function bestFriend(friendNames){
+
+function bestfriends(friends){
+    const max = friends[0];
+    for(let friend of friends){
+        // console.log(friend);
+        if(friend.length>max.length){
+            max = friend;
+        }
+    }
+    return max;
+}
+const friendsName =bestfriends(['farhana','nimki','adrita','tirtho']);
+console.log(friendsName);
+
+
+/* function bestFriend(friendNames){
 
     let max = friendNames[0];
      
@@ -17,4 +32,4 @@ console.log(bestFriend(['nim','Farhana','binte','THasan','Tazakistan']));
 
 
 let namesIs ="Tazakistan";
-console.log(namesIs.length);
+console.log(namesIs.length); */
