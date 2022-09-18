e579fb2f1f927b410bc1c122edc1573fdbc22104
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-18 23:22:59 +0600

    try to codeing uppercase or lowercase

--- case-convert.js
+++ case-convert.js
@@ -1,6 +1,12 @@
-const userInput = 'blackPinK';
+const userName = 'Farhana';
+const userInput = 'farhaNa';
+if(userName.toLowerCase() == userInput.toLowerCase()){
+    console.log(userInput);
+}
+
+/* const userInput = 'blackPinK';
 const namePro ='blackpink';
 // console.log(namePro.toUpperCase());
 if(userInput.toUpperCase() == namePro.toUpperCase()){
     console.log('user exists');
-}
+} */
