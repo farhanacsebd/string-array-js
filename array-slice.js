27ae0a95870ba77d667abb99847f23f955fb3680
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-24 23:35:07 +0600

    added splice

--- array-slice.js
+++ array-slice.js
@@ -1,3 +1,5 @@
 const number1 = [33,534,24,76,89,90,06,46];
-console.log(number1.slice(3,5));
-console.log(number1.splice(3,5));
+// console.log(number1.slice(3,5));
+// console.log(number1.splice(3,5));
+console.log(number1.splice(3,2,222,336,553,2178));
+console.log(number1);
