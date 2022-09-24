b5f2ffdf7c817b6bccdb4b40a66bc571eab92367
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-24 23:27:47 +0600

    slice and splice

---
+++ array-slice.js
@@ -0,0 +1,3 @@
+const number1 = [33,534,24,76,89,90,06,46];
+console.log(number1.slice(3,5));
+console.log(number1.splice(3,5));
--- concat.js
+++ concat.js
@@ -1,3 +1,3 @@
 const first1 = [2,4,6,7,2,3];
 const second = [55,44,24,78,61]
-console.log(first1.concat(second));
+// console.log(first1.concat(second));
