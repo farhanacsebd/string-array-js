d28323e7e44e3d5d44925e04e61c770e59a9895f
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-25 23:52:52 +0600

    function of arguments

--- arguments.js
+++ arguments.js
@@ -1,8 +1,11 @@
 function result(num1,num2){
-    console.log(argumen);
-    let sum=[0];
-    sum = num1 + num2;
-    return sum;
+    // console.log(arguments[1]);
+    for(num of arguments){
+        console.log(num);
+    }
+    // let sum=[0];
+    // sum = num1 + num2;
+    // return sum;
 }
 
 console.log(result(33,22,10));
