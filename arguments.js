e36ccd1777307529e3099ac5301b2485876c68c6
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-26 00:08:24 +0600

    arguments correct code

--- arguments.js
+++ arguments.js
@@ -1,6 +1,6 @@
 function result(num1,num2){
     // console.log(arguments[1]);
-    let sum=[0];
+    let sum=0;
     for(let num of arguments){
         sum =sum + num;
     }
@@ -8,4 +8,4 @@ function result(num1,num2){
     return sum;
 }
 
-console.log(result(33,22,10));
+console.log(result(33,22,10,6,8,12));
