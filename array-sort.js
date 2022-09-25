97e41730117043a2621535204050e79e9a922801
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-25 23:17:54 +0600

    array sorting

---
+++ array-sort.js
@@ -0,0 +1,11 @@
+const arr1 = [6,9,8,1,2,5,3,7,4,0]
+// console.log(arr1.sort());
+const arr2 = ['kabir','jarim','kikur','ayara','bib']
+console.log(arr2.sort());
+
+
+const arr3 =[22,53,767,234,43,15,75,89];
+arr3.sort(function(a,b){
+    return a-b;
+})
+console.log(arr3);
